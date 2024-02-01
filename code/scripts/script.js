4ca048559a5e24dfbb9cc6dcfc3f2e2803e3306b
wafi <chrwafi@gmail.com>
2024-02-01 17:08:13 +0100

    box modification & final touch

--- code/scripts/script.js
+++ code/scripts/script.js
@@ -1,16 +1,16 @@
 function showLoginPopup() {
-  document.getElementById('home-section').style.display = 'none';
-  document.getElementById('login-popup').style.display = 'block';
-  document.getElementById('register-popup').style.display = 'none';
-
+  document.getElementById("main-left").style.display = "none";
+  document.getElementById("login-popup").style.display = "block";
+  document.getElementById("register-popup").style.display = "none";
 }
 
 function showRegisterPopup() {
-  document.getElementById('login-popup').style.display = 'none';
-  document.getElementById('register-popup').style.display = 'block';
+  document.getElementById("main-left").style.display = "none";
+  document.getElementById("login-popup").style.display = "none";
+  document.getElementById("register-popup").style.display = "block";
 }
 
 function closePopup(popupId) {
-  document.getElementById(popupId).style.display = 'none';
-  document.getElementById('home-section').style.display = 'block';
+  document.getElementById(popupId).style.display = "none";
+  document.getElementById("main-left").style.display = "flex";
 }
